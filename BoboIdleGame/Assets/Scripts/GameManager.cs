a73b641fea414cd5af9809e9663af2690207fa57
Bobo <32930338+BoboMJZhou@users.noreply.github.com>
2020-07-07 22:38:02 +0800

    add click +coin
    click button to plus coins and show the amount of coins

---
+++ BoboIdleGame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Text coinsText;
+    public double coins;
+
+    public void Start()
+    {
+        coins = 1;
+    }
+
+    public void Update()
+    {
+        coinsText.text = "Coins: " + coins.ToString();
+    }
+
+    public void Click()
+    {
+        coins += 1;
+    }
+}
